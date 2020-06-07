48969659c7f46cf79e972882456b2f01e27c60be
Kauan Rakoski <kauan.k.rakoski@gmail.com>
2020-06-07 19:22:01 -0300

    update

--- mom_send.py
+++ mom_send.py
@@ -1,15 +1,18 @@
-from twilio.rest import Client 
+from twilio.rest import Client
+import datetime
  
 account_sid = 'AC970aa9aa62f3d0907eae384d484cdb09' 
 auth_token = 'e5533d6fddd2da471381a994b0ab1331' 
 client = Client(account_sid, auth_token) 
 
+dia = str(datetime.datetime.today())
+
 
 def send_text():
     message = client.messages.create(
-                              from_='whatsapp:+555599309399',
-                              body='Oi. Tudo certo? Comigo e com o bê está tudo certo...',
-                              to='whatsapp:+555596472333'
+                              from_='whatsapp:+14155238886',
+                              body=f'Atualizando...{dia}',
+                              to='whatsapp:+555599309399'
                           )
 
     print(message.sid)
