df8161eec388e7b5ff92aad720adc0989609a706
Kauan Rakoski <kauan.k.rakoski@gmail.com>
2020-06-08 20:59:20 -0300

    lembretes

--- mom_send.py
+++ mom_send.py
@@ -1,18 +1,24 @@
 from twilio.rest import Client
-import datetime
+
  
 account_sid = 'AC970aa9aa62f3d0907eae384d484cdb09' 
 auth_token = 'e5533d6fddd2da471381a994b0ab1331' 
 client = Client(account_sid, auth_token) 
 
-dia = str(datetime.datetime.today())
-
 
 def send_text():
-    message = client.messages.create(
+    bomdia = client.messages.create(
                               from_='whatsapp:+14155238886',
-                              body=f'Atualizando...{dia}',
+                              body=f'Bom dia.',
                               to='whatsapp:+555599309399'
                           )
 
-    print(message.sid)
+    print(bomdia.sid)
+
+    lembretes = client.messages.create(
+        from_='whatsapp:+14155238886',
+        body=f'Lembre-se de escovar os dentes do Bernardo, ler e treinar desenho.',
+        to='whatsapp:+555599309399'
+    )
+
+    print(lembretes.sid)
--- timersetsend.py
+++ timersetsend.py
@@ -5,6 +5,6 @@ from mom_send import send_text
 sched = BlockingScheduler()
 
 # Schedule job_function to be called every two hours
-sched.add_job(send_text, 'interval', hours=4)
+sched.add_job(send_text, 'interval', hours=2)
 
 sched.start()
